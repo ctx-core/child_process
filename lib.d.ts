09e711e58b45d8f1b31f9ab776b76d4c67e5b754
Brian Takita <brian.takita@gmail.com>
2020-03-03 22:58:44 -0500

    fix: _spawn__pipe,spawn__pipe,pipe__stdout_and_stderr: cmd is a ChildProcessWithoutNullStreams

--- lib.d.ts
+++ lib.d.ts
@@ -1,4 +1,6 @@
-export declare function pipe__stdout_and_stderr(cmd: any): void;
+/// <reference types="node" />
+import { ChildProcessWithoutNullStreams } from 'child_process';
+export declare function pipe__stdout_and_stderr(cmd: ChildProcessWithoutNullStreams): void;
 export declare function _spawn__pipe(pipe?: typeof pipe__stdout_and_stderr): (cmd_name: any, argv: any, pipe__override?: typeof pipe__stdout_and_stderr) => Promise<unknown>;
 export declare const spawn__pipe: (cmd_name: any, argv: any, pipe__override?: typeof pipe__stdout_and_stderr) => Promise<unknown>;
 export declare const _cmd__spawn: (cmd_name: any, argv: any, pipe__override?: typeof pipe__stdout_and_stderr) => Promise<unknown>;
--- lib.ts
+++ lib.ts
@@ -1,11 +1,11 @@
-import { spawn } from 'child_process'
-export function pipe__stdout_and_stderr(cmd) {
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
+export function pipe__stdout_and_stderr(cmd:ChildProcessWithoutNullStreams) {
 	cmd.stdout.pipe(process.stdout)
 	cmd.stderr.pipe(process.stderr)
 }
 export function _spawn__pipe(pipe = pipe__stdout_and_stderr) {
 	return function spawn__pipe(cmd_name, argv, pipe__override = pipe) {
-		const cmd = spawn(cmd_name, argv)
+		const cmd:ChildProcessWithoutNullStreams = spawn(cmd_name, argv)
 		pipe__override(cmd)
 		return new Promise((resolve, reject)=>{
 			cmd.on('close', code=>{
