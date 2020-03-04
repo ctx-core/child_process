5871afe5a51da444a337a64a1264e1b0739eccc8
Brian Takita <brian.takita@gmail.com>
2020-03-04 15:19:30 -0500

    feat: capture__child_process
    ChildProcess instead of ChildProcessWithoutNullStreams
    
    ∋ "memory-streams": "^0.1.3"

--- lib.d.ts
+++ lib.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="node" />
-import { ChildProcessWithoutNullStreams } from 'child_process';
-export declare type Receive__child_process = (child_process: ChildProcessWithoutNullStreams) => void;
-export declare function pipe__stdout_and_stderr(child_process: ChildProcessWithoutNullStreams): void;
-export declare type Pipe__child_process = (child_process: ChildProcessWithoutNullStreams, pipe?: Receive__child_process) => Promise<number>;
+import { ChildProcess } from 'child_process';
+export declare type Receive__child_process = (child_process: ChildProcess) => void;
+export declare function pipe__stdout_and_stderr(child_process: ChildProcess): void;
+export declare type Pipe__child_process = (child_process: ChildProcess, pipe?: Receive__child_process) => Promise<number>;
 export declare function _pipe__child_process(pipe?: Receive__child_process): Pipe__child_process;
 export declare const pipe__child_process: Pipe__child_process;
 export declare function receive__child_process(fn: Receive__child_process): Receive__child_process;
+export declare function capture__child_process(child_process: ChildProcess): Promise<string>;
--- lib.ts
+++ lib.ts
@@ -1,20 +1,21 @@
-import { ChildProcessWithoutNullStreams } from 'child_process'
+import { ChildProcess } from 'child_process'
+import { WritableStream } from 'memory-streams'
 export type Receive__child_process =
-	(child_process:ChildProcessWithoutNullStreams)=>void
-export function pipe__stdout_and_stderr(child_process:ChildProcessWithoutNullStreams) {
-	child_process.stdout.pipe(process.stdout)
-	child_process.stderr.pipe(process.stderr)
+	(child_process:ChildProcess)=>void
+export function pipe__stdout_and_stderr(child_process:ChildProcess) {
+	child_process.stdout?.pipe(process.stdout)
+	child_process.stderr?.pipe(process.stderr)
 }
 export type Pipe__child_process =
 	(
-		child_process:ChildProcessWithoutNullStreams,
+		child_process:ChildProcess,
 		pipe?:Receive__child_process
 	)=>Promise<number>
 export function _pipe__child_process(
 	pipe:Receive__child_process = pipe__stdout_and_stderr
 ) {
 	return function pipe__child_process(
-		child_process:ChildProcessWithoutNullStreams,
+		child_process:ChildProcess,
 		pipe__override:Receive__child_process = pipe
 	) {
 		pipe__override(child_process)
@@ -32,4 +33,19 @@ export function _pipe__child_process(
 export const pipe__child_process:Pipe__child_process = _pipe__child_process()
 export function receive__child_process(fn:Receive__child_process) {
 	return fn
+}
+export async function capture__child_process(child_process:ChildProcess) {
+	const stdout = new WritableStream()
+	const stderr = new WritableStream()
+	try {
+		await pipe__child_process(
+			child_process,
+			child_process=>{
+				child_process.stdout?.pipe(stdout)
+				child_process.stderr?.pipe(stderr)
+			})
+	} catch {
+		throw stderr.toString().trim()
+	}
+	return stdout.toString().trim()
 }
