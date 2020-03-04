e6f0a2314e6717495af89e7696f10738a8d1e0dc
Brian Takita <brian.takita@gmail.com>
2020-03-03 22:15:05 -0500

    fix: lib.d.ts

--- lib.d.ts
+++ lib.d.ts
@@ -1,4 +1,4 @@
 export declare function pipe__stdout_and_stderr(cmd: any): void;
-export declare function _spawn__pipe(pipe?: typeof pipe__stdout_and_stderr): (cmd_name: any, argv: any) => Promise<unknown>;
-export declare const spawn__pipe: (cmd_name: any, argv: any) => Promise<unknown>;
-export declare const _cmd__spawn: (cmd_name: any, argv: any) => Promise<unknown>;
+export declare function _spawn__pipe(pipe?: typeof pipe__stdout_and_stderr): (cmd_name: any, argv: any, pipe__override?: typeof pipe__stdout_and_stderr) => Promise<unknown>;
+export declare const spawn__pipe: (cmd_name: any, argv: any, pipe__override?: typeof pipe__stdout_and_stderr) => Promise<unknown>;
+export declare const _cmd__spawn: (cmd_name: any, argv: any, pipe__override?: typeof pipe__stdout_and_stderr) => Promise<unknown>;
