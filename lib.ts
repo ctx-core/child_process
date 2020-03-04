1c7515b1f01c7ae2c1d9831fc1550524fc17fda9
Brian Takita <brian.takita@gmail.com>
2020-03-03 22:05:00 -0500

    feat: + _spawn__pipe

--- lib.ts
+++ lib.ts
@@ -3,17 +3,20 @@ export function pipe__stdout_and_stderr(cmd) {
 	cmd.stdout.pipe(process.stdout)
 	cmd.stderr.pipe(process.stderr)
 }
-export function spawn__pipe(cmd_name, argv, pipe = pipe__stdout_and_stderr) {
-	const cmd = spawn(cmd_name, argv)
-	pipe(cmd)
-	return new Promise((resolve, reject)=>{
-		cmd.on('close', code=>{
-			if (code) {
-				reject(code)
-			} else {
-				resolve(code)
-			}
+export function _spawn__pipe(pipe = pipe__stdout_and_stderr) {
+	return function (cmd_name, argv) {
+		const cmd = spawn(cmd_name, argv)
+		pipe(cmd)
+		return new Promise((resolve, reject)=>{
+			cmd.on('close', code=>{
+				if (code) {
+					reject(code)
+				} else {
+					resolve(code)
+				}
+			})
 		})
-	})
+	}
 }
+export const spawn__pipe = _spawn__pipe()
 export const _cmd__spawn = spawn__pipe
