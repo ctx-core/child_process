c7156896cbb3b43b003cd15de51bd6fdf4352803
Brian Takita <brian.takita@gmail.com>
2020-03-04 00:15:05 -0500

    feat: + receive__child_process: kludge to work around typescript compile error

--- lib.d.ts
+++ lib.d.ts
@@ -5,3 +5,4 @@ export declare function pipe__stdout_and_stderr(child_process: ChildProcessWitho
 export declare type Pipe__child_process = (child_process: ChildProcessWithoutNullStreams, pipe?: Receive__child_process) => Promise<number>;
 export declare function _pipe__child_process(pipe?: Receive__child_process): Pipe__child_process;
 export declare const pipe__child_process: Pipe__child_process;
+export declare function receive__child_process(fn: Receive__child_process): Receive__child_process;
--- lib.ts
+++ lib.ts
@@ -1,12 +1,22 @@
 import { ChildProcessWithoutNullStreams } from 'child_process'
-export type Receive__child_process = (child_process:ChildProcessWithoutNullStreams)=>void
+export type Receive__child_process =
+	(child_process:ChildProcessWithoutNullStreams)=>void
 export function pipe__stdout_and_stderr(child_process:ChildProcessWithoutNullStreams) {
 	child_process.stdout.pipe(process.stdout)
 	child_process.stderr.pipe(process.stderr)
 }
-export type Pipe__child_process = (child_process:ChildProcessWithoutNullStreams, pipe?: Receive__child_process) => Promise<number>
-export function _pipe__child_process(pipe:Receive__child_process = pipe__stdout_and_stderr) {
-	return function pipe__child_process(child_process:ChildProcessWithoutNullStreams, pipe__override = pipe) {
+export type Pipe__child_process =
+	(
+		child_process:ChildProcessWithoutNullStreams,
+		pipe?:Receive__child_process
+	)=>Promise<number>
+export function _pipe__child_process(
+	pipe:Receive__child_process = pipe__stdout_and_stderr
+) {
+	return function pipe__child_process(
+		child_process:ChildProcessWithoutNullStreams,
+		pipe__override:Receive__child_process = pipe
+	) {
 		pipe__override(child_process)
 		return new Promise((resolve, reject)=>{
 			child_process.on('close', code=>{
@@ -19,4 +29,7 @@ export function _pipe__child_process(pipe:Receive__child_process = pipe__stdout_
 		})
 	} as Pipe__child_process
 }
-export const pipe__child_process = _pipe__child_process()
+export const pipe__child_process:Pipe__child_process = _pipe__child_process()
+export function receive__child_process(fn:Receive__child_process) {
+	return fn
+}
