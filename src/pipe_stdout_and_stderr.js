dac9d693634786dd81c61a779c67b1ac99436920
Brian Takita <brian.takita@gmail.com>
2021-06-18 03:47:36 -0400

    remove src/*.js & src/*.map

--- src/pipe_stdout_and_stderr.js
+++
@@ -1,7 +0,0 @@
-export function pipe_stdout_and_stderr(child_process) {
-    var _a, _b;
-    (_a = child_process.stdout) === null || _a === void 0 ? void 0 : _a.pipe(process.stdout);
-    (_b = child_process.stderr) === null || _b === void 0 ? void 0 : _b.pipe(process.stderr);
-}
-export { pipe_stdout_and_stderr as pipe__stdout_and_stderr };
-//# sourceMappingURL=src/pipe_stdout_and_stderr.js.map
--- src/receive_child_process.js
+++
@@ -1,5 +0,0 @@
-export function receive_child_process(fn) {
-    return fn;
-}
-export { receive_child_process as receive__child_process };
-//# sourceMappingURL=src/receive_child_process.js.map
