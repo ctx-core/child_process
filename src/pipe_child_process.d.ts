0465f40c33b5ade5e2d61eecb943dbb1e7c8a1f4
Brian Takita <brian.takita@gmail.com>
2021-06-26 23:07:17 -0400

    ## 6.0.9
    ### Patch Changes
    
    - dist directory

--- src/pipe_child_process.d.ts
+++
@@ -1,3 +0,0 @@
-import { pipe_child_process_T } from './pipe_child_process_';
-export declare const pipe_child_process: pipe_child_process_T;
-export { pipe_child_process as pipe, pipe_child_process as pipe__child_process, };
--- src/pipe_child_process_.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import type { ChildProcess } from 'child_process';
-import type { receive_child_process_T } from './receive_child_process';
-export declare function pipe_child_process_(pipe?: receive_child_process_T): pipe_child_process_T;
-export declare type pipe_child_process_T = (child_process: ChildProcess, pipe?: receive_child_process_T) => Promise<number>;
-export { pipe_child_process_ as _pipe__child_process };
